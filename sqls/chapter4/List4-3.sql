55cc6997da8e5c4cd79dab62ee002d05f323a1be
takumiyoshiko <takumiyoshiko@gmail.com>
2022-05-12 23:39:57 +0900

    finish chapter4-4

---
+++ sqls/chapter4/List4-3.sql
@@ -0,0 +1,4 @@
+INSERT INTO ShohinIns (shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka, shiire_tanka, torokubi) VALUES ('0005', '圧力鍋', 'キッチン用品', 6800, 5000, '2009-01-05');
+
+-- 列リストがなくても同じ内容を指す。
+-- INSERT INTO ShohinIns VALUES ('0005', '圧力鍋', 'キッチン用品', 6800, 5000, '2009-01-05');

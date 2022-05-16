72a1e43b640f564d9e8a4fecee9a7267d2a1cc91
takumiyoshiko <takumiyoshiko@gmail.com>
2022-05-16 09:42:10 +0900

    finish chapter4

---
+++ sqls/chapter4/practice/Practice4-3.sql
@@ -0,0 +1,15 @@
+BEGIN TRANSACTION;
+    CREATE TABLE ShohinSaeki
+    (
+        shohin_id CHAR(4) NOT NULL,
+        shohin_mei VARCHAR(100) NOT NULL,
+        hanbai_tanka INTEGER,
+        shiire_tanka INTEGER ,
+        saeki INTEGER ,
+        PRIMARY KEY (shohin_id)
+    );
+
+    INSERT INTO ShohinSaeki (shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, saeki)
+    SELECT shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, hanbai_tanka - shiire_tanka
+    FROM Shohin;
+COMMIT;
---
+++ sqls/chapter4/practice/Practice4-4.sql
@@ -0,0 +1,4 @@
+UPDATE ShohinSaeki
+ SET hanbai_tanka = 4000,
+     saeki = 4000 - shiire_tanka
+ WHERE shohin_mei = 'Tシャツ';
